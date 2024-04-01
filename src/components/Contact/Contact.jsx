085167c2a17e205f8192f0f2ab9063da9535c4b3
Prince Pal <103116418+princee9511@users.noreply.github.com>
2024-04-01 11:46:12 +0530

    first commit

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:princee9511@gmail.com">princee9511@gmail.com</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a href="https://www.linkedin.com/in/prince-pal-a85163233/">linkedin.com/prince</a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://github.com/princee9511/">github.com/prince</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,25 @@
+import styles from "./components/About/Home.css";
+import { About } from "./components/About/About";
+import { Contact } from "./components/Contact/Contact";
+import { Experience } from "./components/Experience/Experience";
+import { Hero } from "./components/Hero/Hero";
+import { Navbar } from "./components/Navbar/Navbar";
+import { Projects } from "./components/Projects/Projects";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+//import { Tictactoe } from "./components/Tictactoe/Board";
+
+
+function Home() {
+  return (
+    <div className={styles.App}>
+      <Navbar />
+      <Hero />
+      <About />
+      <Experience />
+      <Projects />
+      <Contact />
+    </div>
+  );
+}
+
+export default Home;
